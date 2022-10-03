c3d8cb5e61ddb54f59c14e41823bc1ead990c1f7
CJINBNH <alex_egorov_89@mail.ru>
2022-10-03 19:51:41 +0400

    Добавил решение с помощью функции

--- 002.py
+++ 002.py
@@ -2,11 +2,20 @@
 # *Пример:*
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
+# print('Введите целое число: ')
+# N = int(input())
+# list = []
+# fact = 1
+# for i in range(1, N + 1):
+#     fact *= i
+#     list.append(fact)
+# print(list)
+
 print('Введите целое число: ')
 N = int(input())
-list = []
-fact = 1
-for i in range(1, N + 1):
-    fact *= i
-    list.append(fact)
-print(list)
+def fac(x):
+    if x == 0:
+        return 1
+    else:
+        return x * fac(x -1)
+print(fac(N))
